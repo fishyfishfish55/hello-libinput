4e54a12fb57ebe50d4f43a0ef011d610e2e707a2
fishyfishfish55 <l.lacosct@gmail.com>
2023-12-20 13:30:28 -0500

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Introduction to libinput";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgsFor = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+
+      targetSystems = [ "aarch64-linux" "x86_64-linux" ];
+    in {
+      devShells = nixpkgs.lib.genAttrs targetSystems (system:
+        let pkgs = pkgsFor system;
+        in {
+          default = pkgs.mkShell {
+            name = "hello-libinput-devel";
+            nativeBuildInputs = with pkgs; [
+              # Compilers
+              cargo
+              rustc
+              scdoc
+
+              # libs
+              udev
+
+              # Tools
+              pkg-config
+              clippy
+              gdb
+              gnumake
+              rust-analyzer
+              rustfmt
+              strace
+              valgrind
+              zip
+            ];
+          };
+        });
+    };
+}
